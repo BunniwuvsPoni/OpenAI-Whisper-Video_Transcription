1b7ab7c0b681677a9d9c44fab124916b15f38a64
Tony Wang <zichutonywang@gmail.com>
2024-11-18 12:37:03 -0500

    Off to testing...

--- Modules/ffmpeg_operations.py
+++ Modules/ffmpeg_operations.py
@@ -33,3 +33,42 @@ def extract_audio(inputFile, outputFile):
         print("terminated")
 
     ffmpeg.execute()
+
+# This script is intended to: Add subtitles to the Video file
+def transcribe_subtitles(inputFile, outputFile, srtFile):
+    from __future__ import annotations
+
+    from ffmpeg import FFmpeg, Progress
+
+    subtitles = "subtitles='" + srtFile.replace(":/", "\\:/") + "'"
+
+    ffmpeg = (
+        FFmpeg()
+        .input(inputFile)
+        .output(
+            outputFile,
+            vf=subtitles
+        )
+    )
+
+    @ffmpeg.on("start")
+    def on_start(arguments: list[str]):
+        print("arguments:", arguments)
+
+    @ffmpeg.on("stderr")
+    def on_stderr(line):
+        print("stderr:", line)
+
+    @ffmpeg.on("progress")
+    def on_progress(progress: Progress):
+        print(progress)
+
+    @ffmpeg.on("completed")
+    def on_completed():
+        print("completed")
+
+    @ffmpeg.on("terminated")
+    def on_terminated():
+        print("terminated")
+
+    ffmpeg.execute()
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # This is the main Python script executing Function(s)/Module(s) as required for the Application to run
 
 # Main function
+import Modules.ffmpeg_operations
+
+
 def main():
     import Modules.directory_services
     import Modules.openai_whisper
+    import Modules.ffmpeg_operations
+    import os
 
     # Get directory of video(s)
     directory = Modules.directory_services.select_directory()
@@ -11,15 +16,33 @@ def main():
     # Get all .mp4 files
     filteredFiles = Modules.directory_services.filter_directory(directory, '.mp4')
 
-    ###############
-    ### TESTING ###
-    ###############
+    # Creates subdirectories as needed
+    mp3Directory = directory + "/mp3/"
+    os.makedirs(mp3Directory, exist_ok=True)
+    transcriptionDirectory = directory + "/transcription/"
+    os.makedirs(transcriptionDirectory, exist_ok=True)
+    finalDirectory = directory + "/output/"
+    os.makedirs(finalDirectory, exist_ok=True)
 
-    directory = "C:/Users/zichu/Downloads/2024-11-17/video.mp4"
-    
-    print (directory)
+    # Process filtered files
+    for file in filteredFiles:
+        # Extract .mp3
+        inputFile = directory + "/" + file
+        print (inputFile)        
+        extractAudioOutputFile = mp3Directory + file.replace(".mp4", ".mp3")
+        print (extractAudioOutputFile)
 
-    print ("End")
+        Modules.ffmpeg_operations.extract_audio(inputFile, extractAudioOutputFile)
+
+        # Generate .srt files using OpenAi Whisper
+        transcribeAudioOutputFileName = transcriptionDirectory + file.replace(".mp4", ".srt")
+
+        Modules.openai_whisper.transcribe_audio(extractAudioOutputFile, transcriptionDirectory, transcribeAudioOutputFileName)
+
+        # Generate subtitled video file
+        finalOutputFileName = finalDirectory + file.replace(".mp4", "-Processed.mp4")
+
+        Modules.ffmpeg_operations.transcribe_subtitles(inputFile, finalOutputFileName, transcribeAudioOutputFileName)
 
 # Main execution
 if __name__ == "__main__":
